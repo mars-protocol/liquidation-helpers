7fa94aa6bbff1ef4b39b9e593957ea1616dcc46b
Piotr Babel <piotr.babel@gmail.com>
2022-11-17 09:20:34 +0100

    Update gitignore.

--- scripts/build/deploy/base/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { DeploymentConfig, Addresses } from '../../types/config'
-export declare const taskRunner: (config: DeploymentConfig, address: Addresses) => Promise<void>
--- scripts/build/deploy/osmosis/config.d.ts
+++
@@ -1,4 +0,0 @@
-import { DeploymentConfig, Addresses } from '../../types/config'
-export declare const osmosisTestnetConfig: DeploymentConfig
-export declare const osmosisLocalConfig: DeploymentConfig
-export declare const osmosisAddresses: Addresses
